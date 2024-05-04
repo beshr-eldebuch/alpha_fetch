5a10b52b68d7dd73793a8bca8feaafe8a2b0e7be
Beshr Eldebuch <bshr505@gmail.com>
2024-05-04 14:23:55 +0300

    fix: refactor models and add StockPrice models.

--- app/models.py
+++ app/models.py
@@ -1,11 +1,20 @@
-from sqlalchemy import Column, Float, String, Date, DateTime, Integer
+from sqlalchemy import Column, Float, String, Date, DateTime, Integer, ForeignKey, BigInteger
 from .database import Base
+from datetime import datetime
 
 class Stock(Base):
     __tablename__ = "stocks"
 
-    id = Column(Integer, primary_key=True, index=True)
-    symbol = Column(String, index=True)
+    id = Column(Integer, primary_key=True)
+    symbol = Column(String, index=True, unique=True)
     currency = Column(String)
-    close_price = Column(Float)
+    last_refreshed = Column(DateTime, default=datetime.utcnow)
+
+class StockPrice(Base):
+    __tablename__ = "stocks_prices"
+
+    id = Column(Integer, primary_key=True, index=True)
+    stock_id = Column(Integer, ForeignKey("stocks.id"))
+    currency = Column(String)
+    close = Column(Float)
     date = Column(Date, index=True)
